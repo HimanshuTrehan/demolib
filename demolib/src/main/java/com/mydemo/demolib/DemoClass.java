d4fc4dafa0e5324dd0eebe9c776262fcaf9527fc
Himanshu <himanshutrehan2000@gmail.com>
2021-12-22 16:39:29 +0530

    start

---
+++ demolib/src/main/java/com/mydemo/demolib/DemoClass.java
@@ -0,0 +1,11 @@
+package com.mydemo.demolib;
+
+import android.widget.Toast;
+
+public class DemoClass {
+
+    public static void getValue()
+    {
+        System.out.println("Hello i am in lib");
+    }
+}
